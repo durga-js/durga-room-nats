83aad7cbefc39afa0f482487870cc820ff5cf7fb
Christian Blaschke <mail@platdesign.de>
2017-04-03 14:32:14 +0200

    added implementation and a simple test

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 'use strict';
 
+const Nats = require('nats');
+
+
 
 module.exports = class NatsRoomProxy {
+
+  constructor(natsConfig) {
+    this.nats = Nats.connect(natsConfig);
+
+
+  }
+
+  init(server) {
+    server.hook('stop', () => this.nats.close());
+  }
+
+  getRoomTopic(room) {
+    return `durga.rooms.${room.name}.event`;
+  }
+
+  emit(room, event, payload) {
+    this.nats.publish(this.getRoomTopic(room), JSON.stringify({ event, payload }));
+  }
+
+  connect(room) {
+    this.nats.subscribe(this.getRoomTopic(room), msg => {
+      let e = JSON.parse(msg);
+      room._emitLocal(e.event, e.payload);
+    });
+  }
 
 };
---
+++ test/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Code = require('code');
+const expect = Code.expect;
+const Durga = require('durga');
+const NatsRoomProxy = require('../');
+
+describe('Durga room proxy nats', () => {
+
+  let server;
+  beforeEach(() => {
+    server = new Durga.Server({});
+    server.roomProxy('nats', new NatsRoomProxy());
+  });
+
+  afterEach(() => server.stop());
+
+
+  it('should ', (done) => {
+
+    let room = server.room('test', 'nats');
+    let con = server.createConnection();
+
+    let emitted = false;
+
+    con.listen(e => {
+
+      expect(emitted)
+        .to.be.false();
+
+      emitted = true;
+
+      expect(e)
+        .to.equal({
+          type: 'event',
+          event: 'test',
+          payload: { test:123 }
+        });
+
+      done();
+
+    });
+
+    room.join(con);
+
+    room.emit('test', { test:123 });
+
+  });
+
+});
